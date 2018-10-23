a905ae57e96e29fc8d9e91546b727c4333519b3c
Carlo THeunissen <carlotheunissen96@gmail.com>
2018-10-23 16:32:31 +0200

    add reservation model

--- src/main/java/Communication/server/models/ReservationJavaScript.java
+++ src/main/java/Communication/server/models/ReservationJavaScript.java
@@ -5,4 +5,36 @@ public class ReservationJavaScript {
     public String Title;
     public int startHour;
     public int length;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public int getStartHour() {
+        return startHour;
+    }
+
+    public void setStartHour(int startHour) {
+        this.startHour = startHour;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
 }
