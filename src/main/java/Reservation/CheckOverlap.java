489623251eca73834dec58f4c2972d6be228cd38
AlextheGreat1509 <Alexfacebook1509@gmail.com>
2018-11-06 10:45:15 +0100

    Fixed checking for meeting overlap

--- src/main/java/Reservation/CheckOverlap.java
+++ src/main/java/Reservation/CheckOverlap.java
@@ -15,9 +15,11 @@ public class CheckOverlap {
             if (reservation.getId() == reservationId && reservation.getHasStarted()) {
 
                 for (int i = 0; i < reservations.size(); i++) {
-                    if (reservation.getStart().isBefore(reservations.get(i).getStart()) && reservation.getStart().isBefore(reservations.get(i).getEnd()) && reservation.getEnd().isBefore(reservations.get(i).getStart())) {
-                        reservation.Changed(changed);
-                        return true;
+                    if (!(reservation.getId() == reservations.get(i).getId())) {
+                        if (reservation.getStart().isBefore(reservations.get(i).getStart()) && reservation.getStart().isBefore(reservations.get(i).getEnd()) && reservation.getEnd().isBefore(reservations.get(i).getStart())) {
+                            reservation.Changed(changed);
+                            return true;
+                        }
                     }
                 }
             }
@@ -31,17 +33,18 @@ public class CheckOverlap {
         ArrayList<Reservation> reservations = collection.getRoom(roomId).getReservations();
         for (Reservation reservation : reservations) {
             if (reservation.getId() == reservationId && reservation.getHasStarted()) {
-
                 for (int i = 0; i < reservations.size(); i++) {
-                    if (reservation.getStart().isBefore(reservations.get(i).getStart()) && reservation.getStart().isBefore(reservations.get(i).getEnd()) && reservation.getEnd().isBefore(reservations.get(i).getStart())) {
-                        reservation.setEnd(LocalDateTime.now().plusMinutes(extensionMinutes));
-                        reservation.Changed(changed);
-                        return true;
+                    if (!(reservation.getId() == reservations.get(i).getId())) {
+                        if (reservation.getStart().isBefore(reservations.get(i).getStart()) && reservation.getStart().isBefore(reservations.get(i).getEnd()) && reservation.getEnd().isBefore(reservations.get(i).getStart())) {
+                            reservation.setEnd(LocalDateTime.now().plusMinutes(extensionMinutes));
+                            reservation.Changed(changed);
+                            return true;
 
+                        }
                     }
                 }
-
             }
+
         }
         return false;
     }
