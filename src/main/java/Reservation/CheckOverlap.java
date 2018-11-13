8c5f3255e650120ecfe1309db5591e74ffdeafb7
AlextheGreat1509 <Alexfacebook1509@gmail.com>
2018-11-13 09:42:31 +0100

    Improved code quality in overlapchecker

--- src/main/java/Reservation/CheckOverlap.java
+++ src/main/java/Reservation/CheckOverlap.java
@@ -5,52 +5,51 @@ import java.util.ArrayList;
 
 public class CheckOverlap {
 
-    public boolean CheckOverlap(int reservationId, int roomId){
+    public boolean CheckOverlap(int reservationId, int roomId) {
         ReservationProvider reservationProvider = ReservationProvider.getInstance();
         RoomCollection collection = reservationProvider.getCollection();
         ArrayList<Reservation> reservations = collection.getRoom(roomId).getReservations();
-        Boolean overlap = false;
         for (Reservation reservation : reservations) {
             if (reservation.getId() == reservationId) {
-
-                for (int i = 0; i < reservations.size(); i++) {
-                    if (!(reservation.getId() == reservations.get(i).getId())) {
-                        if (reservation.getStart().isBefore(reservations.get(i).getStart()) && reservation.getEnd().isBefore(reservations.get(i).getStart())) {
-                            overlap = false;
-                        } else if(reservation.getStart().isAfter(reservations.get(i).getEnd())){
-                            overlap = false;
-                        } else{
-                            return true;
-                        }
-                    }
+                if (reservationChecker(reservations, reservation)) {
+                    return true;
                 }
             }
         }
-        return overlap;
+        return false;
     }
 
-    public boolean CheckOverlapAddTime(int reservationId, int extensionMinutes, int roomId, Changed changed){
+    public boolean CheckOverlapAddTime(int reservationId, int extensionMinutes, int roomId, Changed changed) {
         ReservationProvider reservationProvider = ReservationProvider.getInstance();
         RoomCollection collection = reservationProvider.getCollection();
         ArrayList<Reservation> reservations = collection.getRoom(roomId).getReservations();
-        boolean overlap = false;
         for (Reservation reservation : reservations) {
             if (reservation.getId() == reservationId && reservation.getHasStarted()) {
-                for (int i = 0; i < reservations.size(); i++) {
-                    if (!(reservation.getId() == reservations.get(i).getId())) {
-                        if (reservation.getStart().isBefore(reservations.get(i).getStart()) && reservation.getEnd().plusMinutes(extensionMinutes).isBefore(reservations.get(i).getStart())) {
-                            overlap = false;
-                        } else {
-                            return true;
-                        }
-
-                    }
+                if (reservationChecker(reservations, reservation)) {
+                    return true;
                 }
                 reservation.setEnd(reservation.getEnd().plusMinutes(extensionMinutes));
                 reservation.Changed(changed);
             }
         }
 
+        return false;
+    }
+
+    public boolean reservationChecker(ArrayList<Reservation> reservations, Reservation reservation) {
+        boolean overlap = false;
+        for (int i = 0; i < reservations.size(); i++) {
+            if (!(reservation.getId() == reservations.get(i).getId())) {
+                if (reservation.getStart().isBefore(reservations.get(i).getStart()) && reservation.getEnd().isBefore(reservations.get(i).getStart())) {
+                    overlap = false;
+                } else if (reservation.getStart().isAfter(reservations.get(i).getEnd())) {
+                    overlap = false;
+                } else {
+                    return true;
+                }
+
+            }
+        }
         return overlap;
     }
 }
